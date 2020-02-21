6af33ab041d55e79fcacc0049a064f6cba7efadd
skrrrm <a@macbook-windows>
2020-02-21 12:01:02 +0100

    Add project files.

---
+++ KEAOpgave 3.6/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace KEAOpgave_3._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonResult
+            // 
+            this.buttonResult.Location = new System.Drawing.Point(124, 166);
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.Size = new System.Drawing.Size(109, 55);
+            this.buttonResult.TabIndex = 0;
+            this.buttonResult.Text = "Click me!";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(119, 65);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(423, 25);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Write a number for a weekday. Zero is first.";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(124, 102);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 31);
+            this.textBoxNumber.TabIndex = 2;
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Location = new System.Drawing.Point(119, 293);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(0, 25);
+            this.labelDay.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label labelDay;
+    }
+}
+
---
+++ KEAOpgave 3.6/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Udarbejd et lille program, 
+ * der udskriver dagen i tekst, 
+ * når der indtastes et ugedagsnummer
+ */
+
+namespace KEAOpgave_3._6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            switch (textBoxNumber.Text)
+            {
+                case "0":
+                    labelDay.Text = "Monday";
+                    break;
+                case "1":
+                    labelDay.Text = "Tuesday";
+                    break;
+                case "2":
+                    labelDay.Text = "Wednesday";
+                    break;
+                case "3":
+                    labelDay.Text = "Thursday";
+                    break;
+                case "4":
+                    labelDay.Text = "Friday";
+                    break;
+                case "5":
+                    labelDay.Text = "Saturday";
+                    break;
+                case "6":
+                    labelDay.Text = "Sunday";
+                    break;
+                default:
+                    labelDay.Text = "WRONG! YOU NUB!";
+                    break;
+            }
+        }
+    }
+}
